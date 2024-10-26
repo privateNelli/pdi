506f4574b757e0bd8413419946a87b95b7ebe95b
privateNelli <kilito.hernandez@gmail.com>
2024-10-25 22:22:08 -0300

    PDI

---
+++ arq_pdi/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout')
+]
